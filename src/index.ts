ebd025ba172ca4d185105e3b2fd000cf198bd9ce
mimu <akito.mimura@nijibox.co.jp>
2023-10-23 13:36:34 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,17 @@ import * as core from "@actions/core";
 type Layer = "major" | "minor" | "patch";
 
 const run = () => {
-  try {
-    const targetLayer = core.getInput("layer") as Layer;
-    const currentVersion = core.getInput("version");
-    const updatedVersion = getUpdatedVersion(targetLayer, currentVersion);
+  console.log("here");
+  // try {
+  const targetLayer = core.getInput("layer") as Layer;
+  const currentVersion = core.getInput("version");
+  const updatedVersion = getUpdatedVersion(targetLayer, currentVersion);
 
-    core.info(updatedVersion);
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
-  }
+  console.log(targetLayer, currentVersion, updatedVersion);
+  core.setOutput("version", updatedVersion);
+  // } catch (error) {
+  //   if (error instanceof Error) core.setFailed(error.message);
+  // }
 };
 
 const getUpdatedVersion = (layer: Layer, currentVersion: string) => {
